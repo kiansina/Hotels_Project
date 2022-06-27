1e7d9b8f2a299790d3d266d4c63edde3a2c50971
Sina Kian <70090343+kiansina@users.noreply.github.com>
2022-06-27 14:22:58 +0200

    Add files via upload

---
+++ dashboard_hotel.py
@@ -0,0 +1,109 @@
+import dash
+from dash import dcc, html
+import matplotlib
+import matplotlib.pyplot as plt
+import plotly.graph_objs as go
+import numpy as np
+import pandas as pd
+import random
+import locale
+from dash.dependencies import Input, Output
+
+
+cmap = matplotlib.cm.Blues(np.linspace(0,1,40))
+CCC= cmap[4]
+app = dash.Dash()
+
+
+locale.setlocale(locale.LC_ALL, 'it_IT')
+matplotlib.style.use('seaborn')
+df=pd.read_excel(r"C:\Users\sina.kian\Desktop\Ricardo\updates\DataBase_5_final.xlsx")
+
+
+S=['Denominazione Hotel','Indirizzo (Ubicazione Hotel)','Provincia \n(Ubicazione Hotel)','Fabbricato',
+       'Contenuto','Terremoto, Inondazione, Alluvione, Allagamento','Coef. Terremoto', 'Earthquake Zone',  'Earthquake_Risk_Score_Risk_Score', 'Flood_Risk_Score_Risk_Score','Longitude','Latitude','Codice Hotel','Codice_Unico']
+#S=['Codice Hotel','Indirizzo (Ubicazione Hotel)',
+#        'Provincia \n(Ubicazione Hotel)', 'Città (Ubicazione Hotel)',
+#        'Cap (Ubicazione Hotel)','Terremoto, Inondazione, Alluvione, Allagamento','Coef. Terremoto','Earthquake Zone', 'Earthquake_Risk_Score_Risk_Score', 'Flood_Risk_Score_Risk_Score','Longitude','Latitude']
+
+MP=df[['Fabbricato', 'Contenuto']]
+dfs=df[S]
+
+
+LOW=dfs[dfs['Earthquake Zone']==0]
+LOW.sort_values(by=['Terremoto, Inondazione, Alluvione, Allagamento'], inplace=True)
+MEDIUM=dfs[dfs['Earthquake Zone']==1]
+MEDIUM.sort_values(by=['Terremoto, Inondazione, Alluvione, Allagamento'], inplace=True)
+app.layout = html.Div([
+    html.Div([  # this Div contains our scatter plot
+    dcc.Graph(
+        id='mpg_scatter',
+        figure={
+            'data': [go.Scattergeo(
+                                lon = MEDIUM.Longitude,
+                                lat = MEDIUM.Latitude,
+                                text = MEDIUM['Codice_Unico'],
+                                marker_color='green',
+                                marker_size=8
+            )],
+            'layout': go.Layout(
+                title = 'Medium Earthquake zones',
+                hovermode='closest',
+                showlegend = False,
+                plot_bgcolor='black',
+                height=1000,
+                width=1000,
+                geo = dict(
+                    scope='europe',
+                    resolution = 50,
+                    countrycolor = 'blue',
+                    landcolor="#DDDDDD",
+                    showland=True,
+                 )
+            )
+        })],
+    style={'width':'50%','height':'100%', 'float':'left'}),
+         html.Div([  # this Div contains our output graph and vehicle stats
+    dcc.Graph(
+        id='Danni_Diretti',
+        figure={
+            'data': [dict(name='Valore', x=['Fabbricato', 'Contenuto'], y=[0,0],type='bar'),
+            dict(name='Terremoto', x=['Fabbricato', 'Contenuto'], y=[0,0],type='bar'),
+            dict(name='Inondazione, Alluvione, Allagamento', x=['Fabbricato', 'Contenuto'],y=[0,0],type='bar'),
+            dict(name='Terrorismo', x=['Fabbricato', 'Contenuto'], y=[0,0],type='bar'),
+            dict(name='Eventi Socio Politici, Atti Dolosi', x=['Fabbricato', 'Contenuto'], y=[0,0],type='bar')],
+            'layout': go.Layout(
+                title = 'Danni Diretti',
+                barmode='group',
+                 bargroupgap=0.1,
+                 height=1000,
+            )
+        }
+    ),
+
+    ],style={'width':'50%', 'float':'right'})])
+
+@app.callback(
+    Output('Danni_Diretti', 'figure'),
+    [Input('mpg_scatter', 'hoverData')])
+def callback_graph(hoverData):
+    ii =  hoverData['points'][0]['text']
+    i=df[df['Codice_Unico']==ii].index[0]
+    fig = {
+        'data': [dict(name='Valore', x=['Fabbricato', 'Contenuto'], y=MP.loc[i],type='bar'),
+        dict(name='Terremoto', x=['Fabbricato', 'Contenuto'], y=MP.loc[i]*df.loc[i]['Coef. Terremoto'],type='bar'),
+        dict(name='Inondazione, Alluvione, Allagamento', x=['Fabbricato', 'Contenuto'], y=MP.loc[i]*df.loc[i]['Coef.  Inondazione, Alluvione, Allagamento'],type='bar'),
+        dict(name='Terrorismo', x=['Fabbricato', 'Contenuto'], y=MP.loc[i]*df.loc[i]['Coef. Terrorismo'],type='bar'),
+        dict(name='Eventi Socio Politici, Atti Dolosi', x=['Fabbricato', 'Contenuto'], y=MP.loc[i]*df.loc[i]['Coef. Eventi Socio Politici, Atti Dolosi'],type='bar'),
+        ],
+        'layout': go.Layout(
+            title = ii,
+            height = 1000,
+            width=1000,
+        )
+    }
+    return fig
+
+
+
+app.run_server(debug=True, use_reloader=False)
